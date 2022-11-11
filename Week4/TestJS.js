2e51d2ebd27442d47b2360d5bff3096adebf21a6
Sunpet <64309010026@tnk.ac.th>
2022-11-11 13:31:06 +0700

    Create TestJS.js

---
+++ Week4/TestJS.js
@@ -0,0 +1,11 @@
+var myName = 'Sunpet';
+let lastName = 'Pankaeo';
+const age = 20;
+//age = 26;
+var year = 2022;
+let grade = 3.08;
+grade = Math.floor(grade);
+
+console.log(myName);
+console.log(`My name is ${myName} ${lastName}`);
+console.log(grade);

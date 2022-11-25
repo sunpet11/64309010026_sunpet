c5809204bbaa12f33b86fe846b77097f19536c05
Sunpet <64309010026@tnk.ac.th>
2022-11-25 13:27:16 +0700

    Create obj2.js

---
+++ Week6/obj2.js
@@ -0,0 +1,68 @@
+var sum1 = 0
+var sum2 = 0 
+var sum3 = 0
+var sum4 = 0
+var obj=[{
+    "ID":20001,
+    "Name":"นางสาวจันทรรัตน์ ปรีดาศิริกุล",
+    "department":"ฝ่ายบุคคล",
+    "position" : "HR",
+    "salary" : 27000,
+    "Bonus" : "A",
+},{
+    "ID":20002,
+    "Name":"นายภูพัฒน์ วัชรจิระกุล",
+    "department":"ไอที",
+    "position" : "System Engineer",
+    "salary" : 24000,
+    "Bonus" : "S",
+},{
+    "ID":20003,
+    "Name":"นางอนันตญา วัชรจิระกุล",
+    "department":"การเงิน",
+    "position" : "Account",
+    "salary" : 38000,
+    "Bonus" : "B",
+},{
+    "ID":20004,
+    "Name":"นางสาวกันสิตา มิ่งมาลา",
+    "department":"ฝ่ายบุคคล",
+    "position" : "HR",
+    "salary" : 23000,
+    "Bonus" : "A",
+},{
+    "ID":20005,
+    "Name":"นายสุชาติ วัฒนศักดิ์มนตรี",
+    "department":"ฝ่ายบุคคล",
+    "position" : "HR",
+    "salary" : 76000,
+    "Bonus" : "C",
+},{
+    "ID":20006,
+    "Name":"นายกรณ์ อุดมทรัพย์",
+    "department":"การเงิน",
+    "position" : "Account",
+    "salary" : 51000,
+    "Bonus" : "S",
+},{
+    "ID":20007,
+    "Name":"นายจักริน ภูงาม",
+    "department":"ฝ่ายบุคคล",
+    "position" : "HR",
+    "salary" : 19000,
+    "Bonus" : "A",
+},{
+    "ID":20008,
+    "Name":"นางอรัญญา เหล่าสกุล",
+    "department":"เงิน",
+    "position" : "Account",
+    "salary" : 33000,
+    "Bonus" : "C",
+},{
+    "ID":20009,
+    "Name":"นางสาวจสราวลี ธาราวงศ์",
+    "department":"บุคคล",
+    "position" : "HR",
+    "salary" : 27000,
+    "Bonus" : "A",
+}]
